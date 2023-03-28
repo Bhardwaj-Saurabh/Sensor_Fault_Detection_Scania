f54710c759492f768f770e6c7d496fe92ad84777
saurabh <aryan.saurabhbhardwaj@gmail.com>
2023-03-28 19:54:22 +0100

    Define setup file

--- setup.py
+++ setup.py
@@ -1,1 +1,26 @@
 from typing import List
+from setuptools import find_packages, setup
+
+# function to get requirements as a list
+def get_requirements()->List[str]:
+    """
+    Returns list of requirements
+    """
+    requirements = []
+    with open('requirements.txt', 'r') as req:
+        content = req.read()
+
+    for req in content.split('\n'):
+        requirements.append(req)
+
+    return requirements
+
+# setup definition
+setup(
+    name="sensor",
+    version="0.0.1",
+    author="Saurabh Bhardwaj",
+    author_email="aryan.saurabhbhardwaj@gmail.com",
+    packages = find_packages(),
+    install_requires=get_requirements(),
+)
