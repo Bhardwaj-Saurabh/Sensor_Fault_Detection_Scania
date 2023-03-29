cefdc34f03590089616047360b597d270acebb9e
saurabh <aryan.saurabhbhardwaj@gmail.com>
2023-03-29 18:55:21 +0100

    add sensor data, artifact and main utils

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
+from src.configuration.mongo_db_connection import MongoDBClient
+from src.exception import CustomException
+import os,sys
 from src.logger import logging
+from src.pipeline import training_pipeline
+from src.pipeline.training_pipeline import TrainingPipeline
+import os
+from src.utils.main_utils import read_yaml_file
+from src.utils.main_utils import load_object
+import os
 
 
 
 
 def main():
     try:
-        training_pipeline = TrainPipeline()
+        training_pipeline = TrainingPipeline()
         training_pipeline.run_pipeline()
     except Exception as e:
         print(e)
--- src/entity/artifact_entity.py
+++ src/entity/artifact_entity.py
@@ -4,3 +4,5 @@ from dataclasses import dataclass
 class DataIngestionArtifact:
     trained_file_path: str
     test_file_path: str
+
+    
