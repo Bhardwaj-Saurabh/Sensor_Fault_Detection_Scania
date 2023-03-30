111e9cd4991e867548f92c9b44b74f8348f0163c
saurabh <aryan.saurabhbhardwaj@gmail.com>
2023-03-30 14:16:35 +0100

    updated model pusher and training pipeline

--- src/pipeline/training_pipeline.py
+++ src/pipeline/training_pipeline.py
@@ -1,18 +1,21 @@
 
 from src.entity.config_entity import TrainingPipelineConfig,DataIngestionConfig,DataValidationConfig,DataTransformationConfig
 from src.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact,DataTransformationArtifact
-from src.entity.config_entity import ModelTrainerConfig
-from src.entity.artifact_entity import ModelTrainerArtifact
+from src.entity.config_entity import ModelTrainerConfig, ModelEvaluationConfig, ModelPusherConfig
+from src.entity.artifact_entity import ModelTrainerArtifact, ModelEvaluationArtifact, ModelPusherArtifact
 from src.components.data_ingestion import DataIngestion
 from src.components.data_validation import DataValidation
 from src.components.data_transformation import DataTransformation
 from src.components.model_trainer import ModelTrainer
+from src.components.model_evaluation import ModelEvaluation
+from src.components.model_pusher import ModelPusher
 from src.exception import CustomException
 from src.logger import logging
 import os, sys
 
 
 class TrainingPipeline:
+    is_pipeline_running = False
     def __init__(self):
         self.training_pipeline_config = TrainingPipelineConfig()
 
@@ -59,23 +62,39 @@ class TrainingPipeline:
         except Exception as e:
             raise CustomException(e, sys)
         
-    def start_data_evaluation(self):
+    def start_model_evaluation(self, data_validation_artifact:DataValidationArtifact,
+                               model_trainer_artifact:ModelTrainerArtifact):
         try:
-            pass
+            model_eval_config = ModelEvaluationConfig(self.training_pipeline_config)
+            model_eval = ModelEvaluation(model_eval_config, data_validation_artifact, model_trainer_artifact)
+            model_eval_artifact = model_eval.initiate_model_evaluation()
+            return model_eval_artifact
         except Exception as e:
             raise CustomException(e, sys)
         
-    def start_model_pusher(self):
+    def start_model_pusher(self,model_eval_artifact:ModelEvaluationArtifact):
         try:
-            pass
-        except Exception as e:
-            raise CustomException(e, sys)
+            model_pusher_config = ModelPusherConfig(training_pipeline_config=self.training_pipeline_config)
+            model_pusher = ModelPusher(model_pusher_config, model_eval_artifact)
+            model_pusher_artifact = model_pusher.initiate_model_pusher()
+            return model_pusher_artifact
+        except  Exception as e:
+            raise  CustomException(e,sys)
         
     def run_pipeline(self):
         try:
+            TrainingPipeline.is_pipeline_running=True
+
             data_ingestion_artifact:DataIngestionArtifact = self.start_data_ingestion()
             data_validation_artifact=self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
             data_transformation_artifact=self.start_data_transformation(data_validation_artifact=data_validation_artifact)
             model_trainer_artifact = self.start_model_trainer(data_transformation_artifact)
+            model_eval_artifact = self.start_model_evaluation(data_validation_artifact, model_trainer_artifact)
+            if not model_eval_artifact.is_model_accepted:
+                raise Exception("Trained model is not better than the best model")
+            model_pusher_artifact = self.start_model_pusher(model_eval_artifact)
+
+            TrainingPipeline.is_pipeline_running=False
+
         except Exception as e:
             raise CustomException(e, sys)
